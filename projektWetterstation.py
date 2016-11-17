553ee839f74dabdab4c8e3f921b3df5cc9fa9ce6
Robert Master PC <robert.jonas@gmx.at>
2016-11-17 19:10:56 +0100

    17.11.2016: New class KY-053_Sensor

--- projektWetterstation.py
+++ projektWetterstation.py
@@ -1,17 +1,24 @@
 from Temperatur import Temperatur
+from KY-053_Sensor import KY-053_Sensor as KY053
 import time
 import RPi.GPIO as GPIO
 
 sleepTime = 10
 
-temp = Temperatur()
+#temp = Temperatur()
+ky053 = KY053()
 
 try:
     while True:
-        temp.read()
+        #temp.read()
         #temp.out()
-        temp.save()
-        time.sleep(sleepTime)
+        #temp.save()
+        
+		ky053.read()
+		ky053.out()
+		ky053.save()
+		
+		time.sleep(sleepTime)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
