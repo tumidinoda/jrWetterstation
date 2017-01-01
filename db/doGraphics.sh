b77153e5887421cb61311721b944b942ee44a7d2
robert@raspberry <robert.jonas@gmx.at>
2017-01-02 00:29:23 +0100

    graphic update

--- db/doGraphics.sh
+++ db/doGraphics.sh
@@ -3,29 +3,29 @@ dbpath=$HOME/jrWetterstation/db
 
 #1 Tag Auswertung
 rrdtool graph $dbpath/jrWetter_1d.png --left-axis-format "%4.1lf" -w 900 -h 500 -E -A \
---right-axis 1:1014 --right-axis-format "%4.1lf" \
+--right-axis 1:1000 --right-axis-format "%4.1lf" \
 -t "Zimmertemperatur/Luftdruck last 24h" --end now --start end-1d \
 DEF:temp=$dbpath/jrWetter.rrd:temp0:AVERAGE \
 "LINE2:temp#0000ff:Zimmertemperatur\:" "GPRINT:temp:LAST:%2.1lf\n" \
-DEF:press=$dbpath/jrWetter.rrd:press0:AVERAGE CDEF:druck=press,1014,- \
+DEF:press=$dbpath/jrWetter.rrd:press0:AVERAGE CDEF:druck=press,1000,- \
 "LINE2:druck#ff0000:Luftdruck\:" "GPRINT:press:LAST:%2.1lf\n"
 
 #7 Tage Auswertung
 rrdtool graph $dbpath/jrWetter_7d.png --left-axis-format "%4.1lf" -w 900 -h 500 -E -A \
---right-axis 1:1014 --right-axis-format "%4.1lf" \
+--right-axis 1:1000 --right-axis-format "%4.1lf" \
 -t "Zimmertemperatur/Luftdruck last 7d" --end now --start end-7d \
 DEF:temp=$dbpath/jrWetter.rrd:temp0:AVERAGE \
 "LINE2:temp#0000ff:Zimmertemperatur\:" "GPRINT:temp:LAST:%2.1lf\n" \
-DEF:press=$dbpath/jrWetter.rrd:press0:AVERAGE CDEF:druck=press,1014,- \
+DEF:press=$dbpath/jrWetter.rrd:press0:AVERAGE CDEF:druck=press,1000,- \
 "LINE2:druck#ff0000:Luftdruck\:" "GPRINT:press:LAST:%2.1lf\n"
 
 #30 Tage Auswertung
 rrdtool graph $dbpath/jrWetter_30d.png --left-axis-format "%4.1lf" -w 900 -h 500 -E -A \
---right-axis 1:1014 --right-axis-format "%4.1lf" \
+--right-axis 1:1000 --right-axis-format "%4.1lf" \
 -t "Zimmertemperatur/Luftdruck last 30d" --end now --start end-30d \
 DEF:temp=$dbpath/jrWetter.rrd:temp0:AVERAGE \
 "LINE2:temp#0000ff:Zimmertemperatur\:" "GPRINT:temp:LAST:%2.1lf\n" \
-DEF:press=$dbpath/jrWetter.rrd:press0:AVERAGE CDEF:druck=press,1014,- \
+DEF:press=$dbpath/jrWetter.rrd:press0:AVERAGE CDEF:druck=press,1000,- \
 "LINE2:druck#ff0000:Luftdruck\:" "GPRINT:press:LAST:%2.1lf\n"
 
 #transfer data to webserver
