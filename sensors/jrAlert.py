f6e4b7053383f3376853ec4604b1e4813007e95a
Jonas Robert <robert.jonas@gmx.at>
2017-01-05 00:57:40 +0100

    alert update mail

--- sensors/jrAlert.py
+++ sensors/jrAlert.py
@@ -1,30 +1,26 @@
+import logging
 import rrdtool
-rrdResult = rrdtool.fetch("../db/jrWetter.rrd", 'AVERAGE', '-s -2hr')
-print (rrdResult)
+from jrMail import jrMail
+RDD_FILE = '/home/robert/jrWetterstation/db/jrWetter.rrd'
+
+#myLogger=logging.getLogger('jrWetterstationLogger')
+#myLogger.info('jrAlert started')
+
+rrdResult = rrdtool.fetch(RDD_FILE, 'AVERAGE', '-s -4hr')
 valueList = rrdResult[2]
 diffPress = 0
-oldPress = valueList[0][1]
-numPress = 0
+oldPress = valueList[0][1]    # assuming there is at least one value !  catch exception t.b.d
 for value in valueList:
-    print (str(value[0]) + ' ' + str(value[1]))
-    if value[1] != None:    
+#    print (str(value[0]) + ' ' + str(value[1]))
+    if value[1] is not None:    
        diffPress += (value[1]-oldPress)
        oldPress = value[1]
-       numPress += 1
-
+  
 print("Diff: " + str(diffPress) +
-      " Iter: " + str(numPress) +
-      " Change: " + str(diffPress/numPress))
+      " Average change per hour for last 4 hours: " +
+      str(diffPress/4))   # 4
+
+myMail=jrMail()
+myMail.sendMail('Druckaenderung ueber 4h',str(diffPress/4))
 
 
-
-"""
-for i in range(len(valueList)):
-    valueTupel = valueList[i]
-    temp0 = valueTupel[0]
-    press0 = valueTupel[1]
-    print (str(temp0) + " " + str(press0))
-    diffPress+=(oldPress-press0)
-    print (diffPress)
-    oldPress=press0
-"""
