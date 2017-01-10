1ec81720a80114fb279778c7cc86246948e1670d
Robert Master PC <robert.jonas@gmx.at>
2017-01-10 19:11:25 +0100

    HTML Files Update

--- db/infophp.php
+++
@@ -1,8 +0,0 @@
-<?
-
-echo "<img src='jrWetter_1d.png?hash=" . filemtime('jrWetter_1d.png') . "'>";
-echo "<img src='jrWetter_7d.png?hash=" . filemtime('jrWetter_7d.png') . "'>";
-echo "<img src='jrWetter_30d.png?hash=" . filemtime('jrWetter_30d.png') . "'>";
-echo "<object width="290" height="130"><param name="movie" value="http://www.wunderground.com/swf/pws_mini_rf_nc.swf?station=IGERASDO27&freq=&units=metric&lang=EN" /><embed src="http://www.wunderground.com/swf/pws_mini_rf_nc.swf?station=IGERASDO27&freq=&units=metric&lang=EN" type="application/x-shockwave-flash" width="290" height="130" /></object>"
-
-?>
